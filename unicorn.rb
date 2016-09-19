4b8191a2d990e66f3b541ee3a4484bd9f19f4a7e
Arata Tanaka <tarata43@yahoo.co.jp>
2016-09-19 18:35:26 +0900

    Remove comment

--- unicorn.rb
+++ unicorn.rb
@@ -1,11 +1,10 @@
 @dir = "./"
 
-worker_processes 2 # CPUのコア数に揃える
+worker_processes 2
 working_directory @dir
 
 timeout 300
 listen 8081
 
-# unicornは標準出力には何も吐かないのでログ出力を忘れずに
 stderr_path "#{@dir}log/unicorn.stderr.log"
 stdout_path "#{@dir}log/unicorn.stdout.log"
